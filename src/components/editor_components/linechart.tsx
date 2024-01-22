f9d2ca116f76cea5eb998086df9e3ba8f77e6a21
Thomas Mickley-Doyle <tmickleydoyle@gmail.com>
2024-01-22 14:18:14 -0600

    update

--- src/components/editor_components/linechart.tsx
+++ src/components/editor_components/linechart.tsx
@@ -13,16 +13,40 @@ import { uuid } from "uuidv4";
 
 interface Props {
   className?: string;
-  tabledata?: { id: number; device_type: string; month: string; count: number }[];
+  tabledata?: TableDataEntry[];
 }
 
+type TableDataEntry = {
+  [key: string]: any;
+};
+
 const transformDataForChart = (tableData: Props["tabledata"], xColumn: string, yColumn: string, seriesColumn: string) => {
+  if (xColumn === null || yColumn === null) return [] as { id: string; data: { x: string; y: number }[] }[];
   const chartData = (tableData ?? []).reduce((acc, entry) => {
-    const existingEntry = acc.find((item) => item && item.id === entry[seriesColumn]);
+    const existingEntry = acc.find((item: { id: string }) => item && item.id === entry[seriesColumn]);
 
     const newDataPoint = { x: entry[xColumn], y: entry[yColumn] };
 
-    if (existingEntry) {
+    console.log('seriesColumn', seriesColumn)
+
+    if (seriesColumn === '' || seriesColumn === null) {
+      const overallSeries = acc.find((series: { id: string; data: { x: string; y: number }[] }) => series.id === "Overall");
+
+      if (overallSeries) {
+        const existingDataPoint = overallSeries.data.find((data: { x: string; y: number }) => data.x === entry[xColumn]);
+
+        if (existingDataPoint) {
+          existingDataPoint.y += entry[yColumn];
+        } else {
+          overallSeries.data.push(newDataPoint);
+        }
+      } else {
+        acc.push({
+          id: "Overall",
+          data: [newDataPoint],
+        });
+      }
+    } else if (existingEntry) {
       existingEntry.data.push(newDataPoint);
     } else {
       acc.push({
@@ -30,7 +54,7 @@ const transformDataForChart = (tableData: Props["tabledata"], xColumn: string, y
         data: [newDataPoint],
       });
     }
-
+  
     return acc;
   }, [] as { id: string; data: { x: string; y: number }[] }[]);
 
@@ -38,14 +62,14 @@ const transformDataForChart = (tableData: Props["tabledata"], xColumn: string, y
 };
 
 function LineChart(props: Props) {
-  const [xColumn, setXColumn] = useState("month");
-  const [yColumn, setYColumn] = useState("count");
-  const [seriesColumn, setSeriesColumn] = useState("device_type");
+  const [xColumn, setXColumn] = useState('');
+  const [yColumn, setYColumn] = useState('');
+  const [seriesColumn, setSeriesColumn] = useState('');
   const [chartData, setChartData] = useState<{ id: string; data: { x: string; y: number }[] }[]>([]);
 
   useEffect(() => {
     const data = transformDataForChart(props.tabledata, xColumn, yColumn, seriesColumn);
-    setChartData(data);
+    setChartData(data as { id: string; data: { x: string; y: number }[] }[]);
   }, [props.tabledata, xColumn, yColumn, seriesColumn]);
 
   const getColumnOptions = () => {
@@ -61,98 +85,103 @@ function LineChart(props: Props) {
         <Button variant="outline">Download Chart</Button>
       </div>
       
-        <Label htmlFor="framework">x-axis</Label>
-        <Select onValueChange={(value) => setXColumn(value)}>
-          <SelectTrigger id="x-axis">
-            <SelectValue placeholder={null}/>
-          </SelectTrigger>
-          <SelectContent position="popper">
-            {getColumnOptions()}
-          </SelectContent>
-        </Select>
-        <Label htmlFor="framework">y-axis</Label>
-        <Select onValueChange={(value) => setYColumn(value)}>
-          <SelectTrigger id="y-axis">
-            <SelectValue placeholder={null} />
-          </SelectTrigger>
-          <SelectContent position="popper">
-            {getColumnOptions()}
-          </SelectContent>
-        </Select>
-        <Label htmlFor="framework">series</Label>
-        <Select onValueChange={(value) => setSeriesColumn(value)}>
-          <SelectTrigger id="series-axis">
-            <SelectValue placeholder={null} />
-          </SelectTrigger>
-          <SelectContent position="popper">
-            {getColumnOptions()}
-          </SelectContent>
-        </Select>
-        <div className="aspect-[10/2]">
-        <ResponsiveLine
-          data={chartData}
-          margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-          xScale={{ type: 'point' }}
-          yScale={{
-              type: 'linear',
-              min: 'auto',
-              max: 'auto',
-              stacked: false,
-              reverse: false
-          }}
-          yFormat=" >-.2f"
-          axisTop={null}
-          axisRight={null}
-          axisBottom={{
-              tickSize: 5,
-              tickPadding: 5,
-              tickRotation: 0,
-                legend: xColumn.toString(),
-              legendOffset: 36,
-              legendPosition: 'middle'
-          }}
-          axisLeft={{
-              tickSize: 5,
-              tickPadding: 5,
-              tickRotation: 0,
-              legend: yColumn.toString(),
-              legendOffset: -40,
-              legendPosition: 'middle'
-          }}
-          pointSize={3}
-          pointColor={{ theme: 'background' }}
-          pointBorderWidth={1}
-          pointBorderColor={{ from: 'serieColor' }}
-          pointLabelYOffset={-12}
-          useMesh={true}
-          legends={[
-              {
-                  anchor: 'bottom-right',
-                  direction: 'column',
-                  justify: false,
-                  translateX: 100,
-                  translateY: 0,
-                  itemsSpacing: 0,
-                  itemDirection: 'left-to-right',
-                  itemWidth: 80,
-                  itemHeight: 20,
-                  itemOpacity: 0.75,
-                  symbolSize: 12,
-                  symbolShape: 'circle',
-                  symbolBorderColor: 'rgba(0, 0, 0, .5)',
-                  effects: [
-                      {
-                          on: 'hover',
-                          style: {
-                              itemBackground: 'rgba(0, 0, 0, .03)',
-                              itemOpacity: 1
+        <div className="grid grid-cols-[85%_15%]">
+          <div className="aspect-[10/4]">
+            <ResponsiveLine
+              data={chartData}
+              margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+              xScale={{ type: 'point' }}
+              yScale={{
+                  type: 'linear',
+                  min: 'auto',
+                  max: 'auto',
+                  stacked: false,
+                  reverse: false
+              }}
+              yFormat=" >-.2f"
+              axisTop={null}
+              axisRight={null}
+              axisBottom={{
+                  tickSize: 5,
+                  tickPadding: 5,
+                  tickRotation: 0,
+                    legend: xColumn.toString(),
+                  legendOffset: 36,
+                  legendPosition: 'middle'
+              }}
+              axisLeft={{
+                  tickSize: 5,
+                  tickPadding: 5,
+                  tickRotation: 0,
+                  legend: yColumn.toString(),
+                  legendOffset: -40,
+                  legendPosition: 'middle'
+              }}
+              pointSize={3}
+              pointColor={{ theme: 'background' }}
+              pointBorderWidth={1}
+              pointBorderColor={{ from: 'serieColor' }}
+              pointLabelYOffset={-12}
+              useMesh={true}
+              legends={[
+                  {
+                      anchor: 'bottom-right',
+                      direction: 'column',
+                      justify: false,
+                      translateX: 100,
+                      translateY: 0,
+                      itemsSpacing: 0,
+                      itemDirection: 'left-to-right',
+                      itemWidth: 80,
+                      itemHeight: 20,
+                      itemOpacity: 0.75,
+                      symbolSize: 12,
+                      symbolShape: 'circle',
+                      symbolBorderColor: 'rgba(0, 0, 0, .5)',
+                      effects: [
+                          {
+                              on: 'hover',
+                              style: {
+                                  itemBackground: 'rgba(0, 0, 0, .03)',
+                                  itemOpacity: 1
+                              }
                           }
-                      }
-                  ]
-              }
-          ]}
-        />
-      </div>
+                      ]
+                  }
+              ]}
+            />
+          </div>
+          <div>
+              <Label htmlFor="framework">x-axis</Label>
+              <Select onValueChange={(value) => setXColumn(value)}>
+                <SelectTrigger id="x-axis">
+                  <SelectValue placeholder={null}/>
+                </SelectTrigger>
+                <SelectContent position="popper">
+                  {getColumnOptions()}
+                </SelectContent>
+              </Select>
+              <Label htmlFor="framework">y-axis</Label>
+              <Select onValueChange={(value) => setYColumn(value)}>
+                <SelectTrigger id="y-axis">
+                  <SelectValue placeholder={null} />
+                </SelectTrigger>
+                <SelectContent position="popper">
+                  {getColumnOptions()}
+                </SelectContent>
+              </Select>
+              <Label htmlFor="framework">series</Label>
+              <Select onValueChange={(value) => setSeriesColumn(value)}>
+                <SelectTrigger id="series-axis">
+                  <SelectValue placeholder={null} />
+                </SelectTrigger>
+                <SelectContent position="popper">
+                  <SelectItem key={uuid()} value={null}>{null}</SelectItem>
+                  {getColumnOptions()}
+                </SelectContent>
+              </Select>
+            </div>
+        </div>
     </>
   );
 }
--- src/components/editor_components/playground.tsx
+++ src/components/editor_components/playground.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-// import { useState } from 'react';
 import Editor from '@monaco-editor/react';
 
 interface Props {
@@ -9,18 +8,12 @@ interface Props {
 }
 
 function Playground(props: Props) {
-  // // const [code, setCode] = useState(`SELECT * FROM devices.device_usage LIMIT 10;`);
-  // const [code, setCode] = useState(props.code || '');
-  // function handleOnChange(value?: string) {
-  //   console.log('value', value)
-  //   setCode(value || '');
-  // }
   return (
       <div className="text-sm font-mono text-gray-900 dark:text-gray-50 code-container">
         <Editor
           className="h-screen"
           defaultLanguage="sql"
-          defaultValue={props.code.trim()}
+          defaultValue={props.code?.trim() ?? ''}
           options={{
             fontSize: 14,
             fontFamily: 'monospace',
